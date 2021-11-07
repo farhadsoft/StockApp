63481b5973898e6f530744f14170d41b1db6a491
Farhad Mammadov <dev@farhad.su>
2021-11-07 21:31:36 +0200

    Add Azure AD B2C

--- Application/Services/SearchService.cs
+++ Application/Services/SearchService.cs
@@ -2,29 +2,28 @@
 using Application.Interfaces;
 using AutoMapper;
 
-namespace Application.Services
+namespace Application.Services;
+
+public class SearchService : ISearchService
 {
-    public class SearchService : ISearchService
+    private readonly IUnitOfWork unitOfWork;
+    private readonly IMapper mapper;
+
+    public SearchService(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        private readonly IUnitOfWork unitOfWork;
-        private readonly IMapper mapper;
+        this.unitOfWork = unitOfWork;
+        this.mapper = mapper;
+    }
 
-        public SearchService(IUnitOfWork unitOfWork, IMapper mapper)
-        {
-            this.unitOfWork = unitOfWork;
-            this.mapper = mapper;
-        }
+    public async Task<IEnumerable<ProductNameDto>> GetAllNames()
+    {
+        var result = await unitOfWork.ProductRepository.GetNamesAsync();
+        return mapper.Map<IEnumerable<ProductNameDto>>(result.DistinctBy(x => x.Name));
+    }
 
-        public async Task<IEnumerable<ProductNameDto>> GetAllNames()
-        {
-            var result = await unitOfWork.ProductRepository.GetNamesAsync();
-            return mapper.Map<IEnumerable<ProductNameDto>>(result.DistinctBy(x => x.Name));
-        }
-
-        public async Task<IEnumerable<ProductSearchDto>> GetByNameAsync(string name)
-        {
-            var result = await unitOfWork.ProductRepository.GetByNameAsync(name);
-            return mapper.Map<IEnumerable<ProductSearchDto>>(result);
-        }
+    public async Task<IEnumerable<ProductSearchDto>> GetByNameAsync(string name)
+    {
+        var result = await unitOfWork.ProductRepository.GetByNameAsync(name);
+        return mapper.Map<IEnumerable<ProductSearchDto>>(result);
     }
 }
